75b99d77bd9fd71f7448cd5370c7d2204c778d95
Your Name <you@example.com>
2024-07-22 13:28:06 +0700

    mebnuat get

--- src/index.js
+++ src/index.js
@@ -2,17 +2,44 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
+const supabase = require("@supabase/supabase-js");
 
 dotenv.config();
 
 const PORT = process.env.PORT;
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_KEY = process.env.SUPABASE_KEY;
+
+const db = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 app.use(cors());
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", async (req, res) => {
+  const DataSupa = await db.from("medium-clone").select();
+
+  console.log("Supabase Instance: ", DataSupa);
+  res.send(DataSupa);
+});
+
+app.get("/article/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  if (!id) {
+    return res.json({
+      id,
+      message: "nilai undefined",
+    });
+  }
+
+  const DataSupa = await db
+    .from("medium-clone")
+    .select("*")
+    .eq("id", id)
+    .single();
+
+  console.log("Supabase Instance: ", DataSupa);
+  res.send(DataSupa);
 });
 
 app.listen(PORT, () => {
