080563c4620ebf069c4f31e57c5382593b98aa45
Your Name <you@example.com>
2024-08-02 21:22:37 +0700

    membuat beberapa endpoint

--- src/index.js
+++ src/index.js
@@ -2,33 +2,37 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
-const supabaseClient = require("@supabase/supabase-js");
+const { createClient } = require("@supabase/supabase-js");
+const path = require("path");
+const fileUpload = require("express-fileupload");
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_KEY = process.env.SUPABASE_KEY;
 
-const supabase = supabaseClient.createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 app.use(cors());
-
 app.use(express.json());
+app.use(fileUpload());
+app.use(express.static("public"));
 
 app.get("/:topic", async (req, res) => {
   const topic = req.params.topic;
   console.log(topic);
   try {
     if (topic === "latest") {
-      const DataSupa = await supabase.from("medium-clone").select();
-      return res.send(DataSupa);
+      const { data, error } = await supabase.from("medium-clone").select("*");
+      return res.json(data);
     }
-    const DataSupa = await supabase
+    const { data, error } = await supabase
       .from("medium-clone")
       .select("*")
       .eq("type", topic);
-    return res.json(DataSupa);
+    if (error) throw error;
+    return res.json(data);
   } catch (error) {
     console.error(error);
     return res.status(500).send("Internal Server Error");
@@ -37,20 +41,18 @@ app.get("/:topic", async (req, res) => {
 
 app.get("/article/:id", async (req, res) => {
   const id = parseInt(req.params.id);
-  if (!id) {
-    return res.json({
-      id,
-      message: "nilai undefined",
-    });
-  }
 
-  const DataSupa = await supabase
+  const { data, error } = await supabase
     .from("medium-clone")
     .select("*")
     .eq("id", id)
     .single();
+  if (error) {
+    console.error(error);
+    return res.status(500).send("Internal Server Error");
+  }
 
-  return res.send(DataSupa);
+  return res.json(data);
 });
 
 app.post("/publish/new-story", async (req, res) => {
@@ -63,47 +65,103 @@ app.post("/publish/new-story", async (req, res) => {
     likes,
     comment,
     date,
-    img_content,
     type,
   } = req.body;
 
-  const DataSupa = await supabase.from("medium-clone").insert({
-    title,
-    description,
-    article,
-    author_name,
-    img_user,
-    likes,
-    comment,
-    date,
-    img_content,
-    type,
+  if (!req.files) return res.status(400).send("No files were uploaded.");
+
+  const file = req.files.img_content;
+  const ext = path.extname(file.name);
+  const filename = file.md5 + ext;
+  const url = `${req.protocol}://${req.get("host")}/images/${filename}`;
+  const allowedTypes = [".png", ".jpg", ".jpeg"];
+
+  if (!allowedTypes.includes(ext.toLowerCase()))
+    return res.status(422).json({ msg: "Invalid image format" });
+
+  if (file.size > 5000000)
+    return res.status(422).send({ msg: "Image size too big" });
+
+  file.mv(`./public/images/${filename}`, async (error) => {
+    if (error) {
+      console.error(error);
+      return res.status(500).send("Error saving image file");
+    }
+
+    const { data, error: insertError } = await supabase
+      .from("medium-clone")
+      .insert({
+        title,
+        description,
+        article,
+        author_name,
+        img_user,
+        likes,
+        comment,
+        date,
+        type,
+        img_content: filename,
+        img_url: url,
+      });
+
+    if (insertError) {
+      console.error(insertError);
+      return res.status(500).send("Error inserting story");
+    }
+
+    res.status(200).json({ data, create: true });
   });
-
-  return res.send(DataSupa);
 });
 
-app.post("/auth/signup", async (req, res) => {
-  const { email, password } = req.body;
-  if (!email && !password) {
-    return res.json({
-      email,
-      password,
-      message: "nilai undefined",
-    });
+app.post("/update/profil-user", async (req, res) => {
+  if (req.files === null) {
+    return res.status(400).json({ msg: "No images uploaded" });
   }
-  console.log(email, password);
-  const auth = await supabase.auth.signUp({ email, password });
+  const { name } = req.body;
 
-  return res.json({ data: auth, created: true });
+  const file = req.files.file;
+  const ext = path.extname(file.name);
+  const filename = file.md5 + ext;
+  const url = `${req.protocol}://${req.get("host")}/images/${filename}`;
+  const allowedTypes = [".png", ".jpg", ".jpeg"];
+
+  if (!allowedTypes.includes(ext.toLowerCase())) {
+    return res.status(422).json({ msg: "Invalid image format" });
+  }
+  if (file.size > 5000000) {
+    return res.status(422).json({ msg: "Image size too big" });
+  }
+
+  file.mv(`./public/images/${filename}`, async (error) => {
+    if (error) {
+      console.error(error);
+      return res.status(500).json({ msg: "Error saving image file" });
+    }
+
+    const { data, error: updateError } = await supabase.from("users").update({
+      name,
+      filename,
+      url,
+    });
+    if (updateError) {
+      console.error(updateError);
+      return res.status(500).send("Error updating profile");
+    }
+
+    return res.json({ data, update: true });
+  });
 });
 
-app.post("/auth/logout", async (req, res) => {
-  const auth = await supabase.auth.signOut();
+app.post("/feature/subcribe", async (req, res) => {
+  const { subscriber, subscribed_to, subscribe_at } = req.body;
 
-  return res.json({ data: auth, logout: true });
+  const data = await supabase
+    .from("subscribes")
+    .insert({ subscriber, subscribed_to, subscribe_at });
+
+  res.json({ data, subscribe: true });
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
